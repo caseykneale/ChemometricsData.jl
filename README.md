ea0ab427b38aecb3683ff20f07ad5b07d5f54ff1
Casey Kneale <ckneale@udel.com>
2020-07-04 13:18:22 -0400

    readme update some cleanup and convenience fns

--- README.md
+++ README.md
@@ -10,10 +10,34 @@ The purpose of this package is to allow a user to have easy access to a consorti
 
 Chemometrics data is widely dispersed, in a variety of file formats, and arbitrary conventions. This complicates matters for people wishing simply to load in some data and try new techniques, run a study, or even do a metanalysis. To ameliorate this, the data maintained by or scraped using this package attempts to bundle everything into an intuitive and Julia ready format that is ready for investigation or personal exploration.
 
+# Example Usage
+## Inspection
+The code base provides some pretty basic commands to let users kind of poke around for data. Maybe you don't know what dataset you want, but you're interested in trying your hand at some chemometrics tasks using mid-infrared spectroscopy. You could do the following:
+
+## Loading
+Of course you can easily crack open the datasets and have them ready to use in a few short lines of code. Below is an example workflow of loading in some data, using some metadata, and generating a plot:
+`Julia
+quadrum_data = load("Fresh_Meats")
+meta_data = meta("Fresh_Meats")
+
+spectra = numeric_columns( gimme_the_data )
+bins = parse.( Float64, names(spectra) )
+bin_display_range = 1:100:length(bins)
+using Plots
+plot(   spectra |> Matrix |> transpose,
+        legend = false,
+        title = "Fresh Meats \n (" * meta_data["URL"] * ")",
+        xlabel = "Wavenumber", ylabel = "Absorbance",
+        xticks = ( bin_display_range, bins[bin_display_range] )
+)
+`
+
 # Liability
 The creators and contributors to this package are not responsible for the outcomes of the use of any data, or code in this repository. This is to be used at ones own risk.
 
 Before redistributing any data accessed via this package (either stored in the git repository or via the packages use) ensure that the permissions for the data allow for this. Just because something is "public domain" does not imply it may be used commercially, or redistributed without author's approval.
+
+Should also be noted that it is possible errors exist in these datasets due to them being manipulated(transposed, file formats changed, etc) to get them into a common form. Please report any and all issues - it's greatly appreciated.
 
 # Submissions
 ## How to Submit
--- src/ChemometricsData.jl
+++ src/ChemometricsData.jl
@@ -6,6 +6,24 @@ module ChemometricsData
     include("manifest.jl")
 
     """
+        suggest_a_dataset(dataset_name_lc)
+
+    if a dataset isn't found try to guess what the user tried to type.
+    Note: this is an internal function.
+    """
+    function suggest_a_dataset( dataset_name )
+        dataset_name_lc = lowercase( dataset_name )
+        println(Crayon(foreground = :red), "Sorry no such dataset found.")
+        db_keys = collect(keys(data_manifest))
+        if length( dataset_name ) > 1
+            scores = [ Levenshtein()(dataset_name_lc, lowercase(k)) for k in db_keys ]
+            println(Crayon(foreground = :yellow), "By any chance did you mean to type?")
+            best_guess = first( sortperm(scores) )
+            println(Crayon(foreground = :yellow, italics = true), "\t " * db_keys[best_guess])
+        end
+    end
+
+    """
         search(keyword::String)::Vector{String}
 
     Enter a keyword, such as "Food", "Forensics", measurement technique, etc, and see what datasets which are available are related too it.
@@ -43,17 +61,58 @@ module ChemometricsData
                 end
             end
         else
-            println(Crayon(foreground = :red), "Sorry no such dataset found.")
-            #ToDo: offer levenshtien suggestions...
-            db_keys = collect(keys(data_manifest))
-            if length( dataset_name ) > 1
-                scores = [ Levenshtein()(dataset_name_lc, lowercase(k)) for k in db_keys ]
-                println(Crayon(foreground = :yellow), "By any chance did you mean to type?")
-                best_guess = first( sortperm(scores) )
-                println(Crayon(foreground = :yellow), "\t - " * db_keys[best_guess])
-            end
+            suggest_a_dataset(dataset_name)
         end
     end
-
     export describe
+
+    function load( dataset_name::String )
+        dataset_name_lc = lowercase( dataset_name )
+        if haskey( data_manifest, dataset_name )
+            println(Crayon(foreground = :blue), "Dataset found.")
+            sug_path = Base.joinpath( DATA_PATH , dataset_name )
+            avail = isdir( sug_path ) ? "Offline" : "Online Only"
+            if avail == "Offline"
+                assets = readdir( sug_path )
+                if length( readdir( sug_path ) ) > 1
+                    data = Dict([ f => DataFrame!(CSV.File(Base.joinpath(sug_path, f) )) for f in assets  ])
+                else
+                    data = DataFrame!(CSV.File(Base.joinpath(sug_path, first(assets) ) ))
+                end
+                println(Crayon(foreground = :blue), "Dataset loaded.")
+                return data
+            else
+                suggest_a_dataset(dataset_name)
+            end
+        end
+        return nothing
+    end
+    export load
+
+    function meta( dataset_name::String )
+        if haskey( data_manifest, dataset_name )
+            return data_manifest[ dataset_name ]
+        else
+            suggest_a_dataset(dataset_name)
+        end
+        return nothing
+    end
+    export meta
+
+    isnumeric(s::String) = tryparse(Float64, s) isa Number
+
+    function numeric_columns( df::DataFrame )
+        cols = String.( names( df ) )
+        numer_cols = Symbol.( cols[ isnumeric.( cols ) ] )
+        return df[!, numer_cols]
+    end
+    export numeric_columns
+
+    function nonnumeric_columns( df::DataFrame )
+        cols = String.( names( df ) )
+        numer_cols = Symbol.( cols[ .!isnumeric.( cols ) ] )
+        return df[!, numer_cols]
+    end
+    export nonnumeric_columns
+
 end
