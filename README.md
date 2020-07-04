9fd9cd6790357121dedda70da2a619e120dbfc16
Casey Kneale <ckneale@udel.com>
2020-07-04 08:44:41 -0400

    README.md and added a some datasets

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
-# ChemometricsData
+# ChemometricsData.jl
 
 [![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://caseykneale.github.io/ChemometricsData.jl/stable)
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://caseykneale.github.io/ChemometricsData.jl/dev)
 [![Build Status](https://github.com/caseykneale/ChemometricsData.jl/workflows/CI/badge.svg)](https://github.com/caseykneale/ChemometricsData.jl/actions)
 [![Coverage](https://codecov.io/gh/caseykneale/ChemometricsData.jl/branch/master/graph/badge.svg)](https://codecov.io/gh/caseykneale/ChemometricsData.jl)
+
+# Overview
+The purpose of this package is to allow a user to have easy access to a consortium of publicly available chemometrics datasets.
+
+Chemometrics data is widely dispersed, in a variety of file formats, and arbitrary conventions. This complicates matters for people wishing simply to load in some data and try new techniques, run a study, or even do a metanalysis (IE: train an autoencoder on data from say all NIR instruments). To amelerioate this, the data maintained or scraped using this package attempts to bundle everything into an intuitive and Julia ready format ready for investigation or personal exploration.
+
+# Liability
+The creators and contributors to this package are not responsible for the outcomes of the use of any data, or code in this repository. This is to be used at ones own risk.
+
+Before redistributing any data accessed via this package (either stored in the git repository or via the packages use) ensure that the permissions for the data allow for this. Just because something is "public domain" does not imply it may be used commercially, or redistributed without author's approval.
+
+# Submissions
+## How to Submit
+There are several ways to contribute to this effort:
+ - Make a Pull Request and submit a dataset! This should update the manifest.jl file and provide the data in a plaintext manner in a seperate folder.
+ - File an issue on the repository and point to where your data is. Give written permission for it's redistribution and some form of proof that you are the legal copyright owner of the data!
+
+## Restrictions
+ - Data should not exceed 10mb. If it does contact us and we will navigate an alternative means of hosting
+ - It is highly preferable that the data is in a tabular file format, such as CSV. Proprietary file formats regularly change (companies go out of business, software gets deprecated, etc).
+ - if the content or the origin of the data is suspect, it will be either not accepted, or removed.
--- src/manifest.jl
+++ src/manifest.jl
@@ -12,7 +12,7 @@ manifest = Dict(
         "format" => "csv",
         "description" => "\"Spectra of 654 pharmaceutical tablets from two Multitab spectrometers (FossNIRsystems, Silverspring, MD) will be the basis of the ShootOut at the next Chambersburg meeting (August 10-16, 2002). All tablets were measured in the transmittance mode.  Each individual tablet was subsequently analyzed for ASSAY value, tablet weight, and tablet hardness. All log (1/T) spectra cover the region from 600 to 1898 nm in 2 nm increments.\"... \"Data from each instrument has been split into a calibration file (155 spectra), and a validation file (459 spectra). In addition, a small test file (40 spectra) from each instrument is avaliable for testing calibrations. The challenge is to develop the best possible calibration for the ASSAY value on one instrument, and to provide the best means of transferring the calibration to the other instrument. The reference method has an error of +/- 1.3 mg on these tablets with a nominal value of 200 mg per tablet. The data set includes tablets with a wide ASSAY range, 152 to 239 mg, for developing calibration(s). No deliberate outliers are included in the data set, but there may be outliers due to experimental errors. The name of the active ingredient in these tablets can not be disclosed for proprietary reasons.\"",
         "URL" => "https://web.archive.org/web/20031204005143/http://www.idrc-chambersburg.org/shootout_2002.htm",
-        "Date Accessed" => "6/3/2020",
+        "Date Accessed" => "7/3/2020",
         "references" => ["Gary E. Ritchie, Chairman NIRVWoG and Sr. Scientist, Pharmaceutical Analysis/New Technology, Purdue Pharma L.P., 444 Saw Mill River Road, Ardsley, NY 10502",
                         "http://www.foss.dk/c/p/solutions/products/showprodfamily.asp?prodfamilypkid=98"],
         "permission" => "Public domain. Given to Dr. Casey Kneale via e-mail in 2018."
@@ -21,7 +21,7 @@ manifest = Dict(
 "Tecator_Meat_Data" =>
     Dict(
         "usage" => ["regression"],
-        "keywords" => ["food", "meat", "IDRC", "shootout"],
+        "keywords" => ["food", "meat"],
         "property values" => ["fat","water","protein"],
         "format" => "csv",
         "measurements" => ["NIR"],
@@ -30,7 +30,7 @@ manifest = Dict(
         "observations" => "240",
         "description" => "Statement of permission from Tecator (the original data source).These data are recorded on a Tecator Infratec Food and Feed Analyzer working in the wavelength range 850 - 1050 nm by the Near Infrared Transmission (NIT) principle. Each sample contains finely chopped pure meat with different moisture, fat and protein contents.If results from these data are used in a publication we want you to mention the instrument and company name (Tecator) in the publication. In addition, please send a preprint of your article to Karin Thente, Tecator AB, Box 70, S-263 21 Hoganas, Sweden. The data are available in the public domain with no responsability from the original data source. The data can be redistributed as long as this permission note is attached. For more information about the instrument - call Perstorp Analytical's representative in your area.",
         "URL" => "https://www.openml.org/d/505",
-        "Date Accessed" => "6/3/2020",
+        "Date Accessed" => "7/3/2020",
         "references" => [""],
         "permission" => "Public domain with statement of permission."
     ),
@@ -48,7 +48,7 @@ manifest = Dict(
         "description" => "This dataset contains 80 NIR measures of corn taken from 3 different instruments( M5, MP5,and MP6 ).
         Measures of NBS glass standards (with replicates) on each instrument are also contained in seperate files.",
         "URL" => "",
-        "Date Accessed" => "6/3/2020",
+        "Date Accessed" => "7/3/2020",
         "references" => [""],
         "permission" => "Public domain. Permission to redistribute given to Dr. Casey Kneale granted by Mike Blackburn 6/3/2020"
     ),
@@ -71,7 +71,7 @@ manifest = Dict(
         total - total aromatics (mass% ASTM D 5186)
         visc - viscosity, cSt(40*C)",
         "URL" => "https://eigenvector.com/wp-content/uploads/2019/06/SWRI_Diesel_NIR_CSV.zip",
-        "Date Accessed" => "6/3/2020",
+        "Date Accessed" => "7/3/2020",
         "references" => [""],
         "permission" => "Public Domain. Collection funded by the US Army."
     ),
@@ -91,7 +91,7 @@ manifest = Dict(
         \"NON-Strawberry\" (diverse collection of other purees, including: strawberry adulterated with other fruits and sugar solutions; raspberry; apple; blackcurrant; blackberry; plum; cherry; apricot; grape juice and mixtures of these.\"
          ",
         "URL" => "https://csr.quadram.ac.uk/example-datasets-for-download/",
-        "Date Accessed" => "6/3/2020",
+        "Date Accessed" => "7/3/2020",
         "references" => ["\"Use of Fourier transform infrared spectroscopy and partial least squares regression for the detection of adulteration of strawberry purees\" Holland JK, Kemsley EK, Wilson RH. (1998). Journal of the Science of Food and Agriculture, 76, 263-269"],
         "permission" => "BBSRC policy on data sharing. Free to analyse and redistribute the data; if the original source is cited."
     ),
@@ -107,7 +107,7 @@ manifest = Dict(
         "observations" => "120",
         "description" => "...\"120 Mid-infrared (MIR) spectra of fresh minced meats – chicken, pork and turkey. Duplicate acquisitions from 60 independent samples. Raw data matrix size [448 x 120]. Obtained using Fourier transform infrared (FTIR) spectroscopy with attenuated total reflectance (ATR) sampling\"",
         "URL" => "https://csr.quadram.ac.uk/example-datasets-for-download/",
-        "Date Accessed" => "6/3/2020",
+        "Date Accessed" => "7/3/2020",
         "references" => ["\"Mid-infrared spectroscopy and authenticity problems in selected meats: a feasibility study\" Al-Jowder O, Kemsley E K, Wilson R. H.(1997) Food Chemistry 59 195-20."],
         "permission" => "BBSRC policy on data sharing. Free to analyse and redistribute the data; if the original source is cited."
     ),
@@ -125,7 +125,7 @@ manifest = Dict(
         Once all the samples had been examined once, a second acquisition session commenced, to produce a second spectrum from each sample.  Again the acquisition order was randomized with respect to country of origin.  thus, duplicate spectra were collected from all samples.
         \"",
         "URL" => "https://csr.quadram.ac.uk/example-datasets-for-download/",
-        "Date Accessed" => "6/3/2020",
+        "Date Accessed" => "7/3/2020",
         "references" => ["\"FTIR spectroscopy and multivariate analysis can distinguish the geographic origin of extra virgin olive oils\" Tapp H.S. et al, J. Agric. Food Chem. 51 (21) 6110-5 (2003) "],
         "permission" => "BBSRC policy on data sharing. Free to analyse and redistribute the data; if the original source is cited."
     ),
@@ -141,9 +141,44 @@ manifest = Dict(
         "observations" => "56",
         "description" => "...\"Mid-infrared (MIR) spectra of authenticated freeze-dried coffee samples (arabica and robusta species, respectively 29 and 27 of each). Raw data matrix size is [286 x 56]. Obtained by Fourier transform infrared (FTIR) spectroscopy with diffuse reflectance (DRIFT) sampling.\"",
         "URL" => "https://csr.quadram.ac.uk/example-datasets-for-download/",
-        "Date Accessed" => "6/3/2020",
+        "Date Accessed" => "7/3/2020",
         "references" => [   "\"Discrimination of Arabica and Robusta in Instant Coffee by Fourier Transform Infrared Spectroscopy and Chemometrics\" Briandet R et al, J. Agric. Food Chem., 44 (1),170–174 (1996)",
                             "\"Near- and Mid-Infrared Spectroscopies in Food Authentication: Coffee Varietal Identification\" Downey G. et al, J. Agric. Food Chem. 45 (11) 4357-4361 (1997)."],
         "permission" => "BBSRC policy on data sharing. Free to analyse and redistribute the data; if the original source is cited."
     )
+
+"Raman_PUFA" =>
+    Dict(
+        "usage" => ["Regression"],
+        "keywords" => ["food", "polyunsaturated", "PUFA", "fat"],
+        "property values" => ["PUFA%emul", "%PUFA"],
+        "format" => "csv",
+        "measurements" => ["Raman"],
+        "spectral range" => "675-1697 cm-1",
+        "spectral resolution" => "1.0 cm-1",
+        "observations" => "69",
+        "description" => " see citation for more details. ",
+        "URL" => "https://data.mendeley.com/datasets/3ympjxywdm/1",
+        "Date Accessed" => "7/4/2020",
+        "references" => [ "Rimal, Raju; Almøy, Trygve; Sæbø, Solve (2019), \"Data for: Comparison of Multi-response Prediction Methods\", Mendeley Data, v1 http://dx.doi.org/10.17632/3ympjxywdm.1 "],
+        "permission" => "CC BY NC 3.0."
+    )
+
+"Dough_NIR" =>
+    Dict(
+        "usage" => ["Regression"],
+        "keywords" => ["food", "dough"],
+        "property values" => ["fat", "sucrose", "flour", "water"],
+        "format" => "csv",
+        "measurements" => ["Raman"],
+        "spectral range" => "1000-2498 nm",
+        "spectral resolution" => "2.0 nm",
+        "observations" => "73",
+        "description" => " see citation for more details. ",
+        "URL" => "https://data.mendeley.com/datasets/3ympjxywdm/1",
+        "Date Accessed" => "7/4/2020",
+        "references" => [ "Rimal, Raju; Almøy, Trygve; Sæbø, Solve (2019), \"Data for: Comparison of Multi-response Prediction Methods\", Mendeley Data, v1 http://dx.doi.org/10.17632/3ympjxywdm.1 "],
+        "permission" => "CC BY NC 3.0."
+    )
+
 )
