dee77ad30ae424c09e47638efeb599b0e0b30280
Casey Kneale <ckneale@udel.com>
2020-10-10 18:12:40 -0400

    added check for CSV

--- src/try_pen_clean.jl
+++ src/try_pen_clean.jl
@@ -2,11 +2,14 @@ using Pkg
 Pkg.activate(".")
 using ChemometricsData
 
+z = ChemometricsData.load("Penicillin_Monitoring")
+
 using CSV, DataFrames
-
 p = "/home/caseykneale/.julia/dev/ChemometricsData/data/Penicillin_Monitoring/100_Batches_IndPenSim_V3.csv"
+z = CSV.read(p; threaded = false)
 
-z = CSV.read(p)
+CSV.File(Base.joinpath(sug_path, f); threaded=false )
+
 
 ChemometricsData.describe("Penicillin_Monitoring")
 
@@ -14,7 +17,6 @@ ChemometricsData.describe("Penicillin_Monitoring")
 ChemometricsData.fetchdata("Penicillin_Monitoring")
 
 
-z = ChemometricsData.load("Penicillin_Monitoring")
 
 
 #using DataFrames
